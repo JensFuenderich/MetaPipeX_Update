6f7952164fee951a6b9d4ff2af8ca4cc28fa178d
JensFuenderich <64157104+JensFuenderich@users.noreply.github.com>
2023-04-06 13:22:47 +0200

    Add files via upload

---
+++ R_Package/R/MetaPipeX-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom DT DTOutput
+#' @importFrom DT renderDT
+#' @importFrom janitor compare_df_cols
+#' @importFrom stats cor
+#' @importFrom stats na.omit
+#' @importFrom stats sd
+## usethis namespace: end
+NULL
